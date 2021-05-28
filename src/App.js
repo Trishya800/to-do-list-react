43b0e3151f7f943121feb406e644f70fd408ef8a
Patrycja Lesniak <patrycja.lesniak@gmail.com>
2021-05-28 13:48:41 +0200

    Form component added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-
+import Form from "./Form";
 
 
 function App() {
   return (
     <main className="container">
+
     <section>
         <h1 className="headline">Lista zadań</h1>
     </section>
 
     <section className="section">
+
         <header className="section__header">
             <h2 className="section__title">Dodaj nowe zadanie</h2>
        </header>
 
         <div className="section__body">
-            <form className="form js-form">
-                <input className="form__input js-newTask" placeholder="Co jest do zrobienia?" />
-                <button className="form__button js-button">Dodaj zadanie</button>
-            </form>
- 
+
+            <Form />
+
         </div>
+
     </section>
+
     <section className="section">
+
         <header className="section__header">
             <h2 className="section__title">Lista zadań</h2>
             <div className="buttons js-buttonsTop"></div>
@@ -31,7 +34,9 @@ function App() {
             <ul className="section__tasks js-tasks"></ul>
             <span className="buttons js-buttonsBottom"></span>
         </div>
+
     </section>
+
 </main>
   );
 }
