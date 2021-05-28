4d9e4d36d444be3c2e46ef2002ad68bdbec48b5e
Patrycja Lesniak <patrycja.lesniak@gmail.com>
2021-05-28 13:45:29 +0200

    basic structure of the app added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+    <section>
+        <h1 className="headline">Lista zadań</h1>
+    </section>
+
+    <section className="section">
+        <header className="section__header">
+            <h2 className="section__title">Dodaj nowe zadanie</h2>
+       </header>
+
+        <div className="section__body">
+            <form className="form js-form">
+                <input className="form__input js-newTask" placeholder="Co jest do zrobienia?" />
+                <button className="form__button js-button">Dodaj zadanie</button>
+            </form>
+ 
+        </div>
+    </section>
+    <section className="section">
+        <header className="section__header">
+            <h2 className="section__title">Lista zadań</h2>
+            <div className="buttons js-buttonsTop"></div>
+        </header>
+
+        <div className="section__body">
+            <ul className="section__tasks js-tasks"></ul>
+            <span className="buttons js-buttonsBottom"></span>
+        </div>
+    </section>
+</main>
   );
 }
 
