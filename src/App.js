577c1ce629c73458ab9ab43a411f8bd244e6cf39
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-06-23 15:21:10 +0200

    useTask added

--- src/App.js
+++ src/App.js
@@ -1,79 +1,36 @@
 import { useState } from "react";
-// import { useTasks } from "./useTask"
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
-import {useLocalStorageState} from "./useLocalStorageState";
+import { useTasks } from "./useTasks";
 
 
 function App() {
-  const [tasks, setTasks] = useLocalStorageState("tasks", "");
-  
   const [hideDone, setHideDone] = useState(false);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
   };
 
-  // const {
-  //   tasks,
-  //   removeTask,
-  //   toggleTaskDone,
-  //   setAllDone,
-  //   addNewTask,
-  //   deleteAllTasks,
-  // } = useTasks();
-
-  const removeTask = (id) => {
-    setTasks(tasks => tasks.filter(task => task.id !== id));
-  };
-
-  const toggleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task => {
-      if (task.id === id) {
-        return { ...task, done: !task.done };
-      }
-
-      return task;
-    }));
-  };
-
-  const setAllDone = () => {
-    setTasks(tasks => tasks.map(task => ({
-      ...task,
-      done: true,
-    })));
-  };
-
-  const addNewTask = (content) => {
-    setTasks(tasks => [
-      ...tasks,
-      {
-        content,
-        done: false,
-        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-      },
-    ]);
-  };
-
-  const deleteAllTasks = () => {
-    setTasks([]);
-  };
+  const {
+    tasks,
+    removeTask,
+    toggleTaskDone,
+    setAllDone,
+    addNewTask,
+    deleteAllTasks,
+  } = useTasks();
 
   return (
     <Container>
       <Header title="Lista zadań" />
-
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form
-          addNewTask={addNewTask}
-        />}
+        body={<Form addNewTask={addNewTask}/>}
       />
-
       <Section
         title="Lista zadań"
         body={
---
+++ src/useTasks.js
@@ -0,0 +1,53 @@
+import { useLocalStorageState } from "./useLocalStorageState";
+
+export const useTasks = () => {
+    const [tasks, setTasks] = useLocalStorageState("tasks", []);
+
+    const removeTask = (id) => {
+        setTasks(tasks => tasks.filter(task => task.id !== id));
+    };
+
+    const toggleTaskDone = (id) => {
+        setTasks(tasks => tasks.map(task => {
+            if (task.id === id) {
+                return {
+                    ...task,
+                    done: !task.done
+                };
+            }
+
+            return task;
+        }));
+    };
+
+    const setAllDone = () => {
+        setTasks(tasks => tasks.map(task => ({
+            ...task,
+            done: true,
+        })));
+    };
+
+    const addNewTask = (content) => {
+        setTasks(tasks => [
+            ...tasks,
+            {
+                content,
+                done: false,
+                id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+            },
+        ]);
+    };
+
+    const deleteAllTasks = () => {
+        setTasks([]);
+    };
+
+    return {
+        tasks,
+        removeTask,
+        toggleTaskDone,
+        setAllDone,
+        addNewTask,
+        deleteAllTasks
+    };
+};
