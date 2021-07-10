16bb795ca1234f75473b8b0a251f881a717f3e92
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-10 23:53:58 +0200

    useQueryParameter, useReplaceQueryParameter added

---
+++ src/features/tasks/TasksPage/queryParameters.js
@@ -0,0 +1,23 @@
+import { useLocation, useHistory } from "react-router";
+
+export const useQueryParameter = ({ param }) => {
+    const location = useLocation();
+    return (
+        new URLSearchParams(location.search)).get(param);
+};
+
+export const useReplaceQueryParameter = () => {
+    const location = useLocation();
+    const history = useHistory();
+
+    return ({ key, value }) => {
+        const searchParams = new URLSearchParams(location.search);
+        if (!value === "") {
+            searchParams.delete(key);
+        } else {
+            searchParams.set(key, value);
+        }
+        history.push(`${location.pathname}?${searchParams.toString()}`);
+    };
+};
+
