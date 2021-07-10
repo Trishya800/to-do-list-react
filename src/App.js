b323aca9d48fd0477e9422807f50fbe248a4cc66
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-10 17:06:19 +0200

    page navigation added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
-import Tasks from "./features/tasks/Tasks";
+import { HashRouter, NavLink, Switch, Route, Redirect } from "react-router-dom";
+import {TasksPage} from "./features/tasks/TasksPage";
+import { AuthorPage } from "./features/tasks/authorPage";
 
-const App = () => <Tasks />;
-
-export default App;
+export const App = () => {
+    return (
+        <HashRouter basename="/to-do-list-react">
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink activeClassName="active" to="/zadania">Zadania</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/autor">O autorze</NavLink>
+                    </li>
+                </ul>
+                <Switch>
+                    <Route path="/zadania">
+                        <TasksPage />
+                    </Route>
+                    <Route path="/autor">
+                        <AuthorPage />
+                    </Route>
+                    <Route path="/">
+                        <Redirect to="/zadania" />
+                    </Route>
+                </Switch>
+            </nav>
+        </HashRouter>
+    )
+};
