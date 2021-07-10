de47e66286a1cd58ab5271b729458a7650f40c47
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-11 00:06:19 +0200

    text abaut author updated

--- src/features/tasks/authorPage/index.js
+++ src/features/tasks/authorPage/index.js
@@ -5,14 +5,13 @@ import Header from "../../../common/Header";
 export const AuthorPage = () => {
     return (
         <Container>
-            <Header title="o autorze" />
+            <Header title="O autorze" />
             <Section
                 title="Patrycja Leśniak"
                 body={
                     <>
-                        <p>Witaj świecie. To ja - Patrycja Niepoprawna optymistka, marzycielka, miłośniczka kotów, psów.Zapalona czytelniczka literatury wszelakiej z upodobaniem do ksiąkowych cegieł. Wieczna poszukiwaczka <b>swojego</b> miejsca
-                            na ziemi.
-                        </p>
+                        Witaj świecie. To ja - <b>Patrycja</b>. Niepoprawna optymistka, marzycielka, miłośniczka kotów, psów. Zapalona czytelniczka literatury wszelakiej z upodobaniem do ksiąkowych cegieł. Wieczna poszukiwaczka <b>swojego</b> miejsca na ziemi.<br />
+                        A jako, że ciągle nie wiem, <b>kim chcę zostać gdy dorosnę</b> pomyślałam, że chcę spróbować w programowanie. No więc jestemi postaram się wyciągnąć z tego kursu ile się da. Dla przyjemności uczę się hiszpańskiego, gram na pianinie, gotuję, strzelam fotki i przeprojektowuję głównie w głowie i na papierze ; mój dom aby pomieścił wszystkie książki jakie chcę przeczytać.
                     </>
                 }
             />
