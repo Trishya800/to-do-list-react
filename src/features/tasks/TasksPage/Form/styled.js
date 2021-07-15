3312b8f0324f3995237f8f6065dc96f1d01110f4
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-15 16:17:20 +0200

    formButton restyling

--- src/features/tasks/TasksPage/Form/styled.js
+++ src/features/tasks/TasksPage/Form/styled.js
@@ -11,19 +11,40 @@ export const FormWrapper = styled.form`
 `;
 
 export const FormButton = styled.button`
-    background: ${({ theme }) => theme.colors.teal};
-    color: ${({ theme }) => theme.colors.white};
+    text-decoration: none;
+    text-align: center;
+    color: ${({ theme }) => theme.colors.teal};
+    border: 1px solid ${({ theme }) => theme.colors.teal};
+
+    line-height: 15px;
     padding: 10px;
-    border: none;
-    transition: 0.5s filter;
+    display: inline-block;
+    font-weight: 600;
+    overflow: hidden;
+    position: relative;
 
     &:hover {
-        filter: brightness(110%);
-        transform: scale(1.01);
+        background-color: ${({ theme }) => theme.colors.teal};
+        color: ${({ theme }) => theme.colors.white};
+        border-bottom: 1px solid ${({ theme }) => theme.colors.teal};
     }
 
-    &:active {
-        filter: brightness(120%);
-        transform: scale(1.01);
+    &:before {
+        content: "";
+        background-color: rgba(255, 255, 255, 0.5);
+        height: 100%;
+        width: 40px;
+        display: block;
+        position: absolute;
+        top: 0;
+        left: -70px;
+        transform: skewX(-45deg) translateX(0px);
+        transition: none;
     }
+
+    &:hover:before {
+        transform: skewX(-45deg) translateX(800px);
+        transition: all 0.6s ease-in;
+    }
+
 `;
